77ef98a3eaf084b445906d856f2d06f2f470e412
ripplefox <lr@ripplefox.com>
2020-04-19 22:20:36 +0800

    update to the latest release

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
   var downloadLink = {
-    'mac': 'https://github.com/ripplefox/download/releases/download/0.2/RippleClientOsx32.rar',
-    'win': 'https://github.com/ripplefox/download/releases/download/0.2/RippleClientWin32.rar',
-    'pdf': 'https://github.com/ripplefox/download/files/43495/RippleJiaoCheng_1.0.pdf'
+    'mac': 'https://github.com/ripplefox/ripplewallet/releases/download/v1.0.0/FoxletRipple-1.0.0-mac-x64.zip',
+    'win': 'https://github.com/ripplefox/ripplewallet/releases/download/v1.0.0/FoxletRipple-1.0.0-win-x86.zip',
+    'pan': 'https://pan.baidu.com/s/1sosDPDtwbMDhwHdz-1Dh7w'
   };
 
   $('#download-btn').on('click', function() {
@@ -13,8 +13,8 @@ $(document).ready(function() {
       document.getElementById('download-btn').action = downloadLink.mac;
     } else if (selected === 'win') {
       document.getElementById('download-btn').action = downloadLink.win;
-    } else if (selected === 'pdf') {
-      document.getElementById('download-btn').action = downloadLink.pdf;
+    } else if (selected === 'pan') {
+      document.getElementById('download-btn').action = downloadLink.pan;
     } else {
       document.getElementById('download-btn').action = downloadLink.linux64;
     }
